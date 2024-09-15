ba5caf8711f96e9c121c80cc323c14357714eef2
Maxim Molyanov <mm_89615@hotmail.com>
2024-09-15 14:48:50 +0300

    hw 2.2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,45 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        result = super().clean()
+        unique_tags = set()
+        non_deleted_count = 0
+        main_tags_count = 0
+        for form in self.forms:
+            if not form.cleaned_data:
+                continue
+            if form.cleaned_data.get('DELETE', False):
+                continue
+            if form.cleaned_data.get('is_main'):
+                main_tags_count += 1
+            non_deleted_count += 1
+            unique_tags.add(form.cleaned_data['tag'])
+        if non_deleted_count != len(unique_tags):
+            raise ValidationError('Теги не должны повторяться')
+        if main_tags_count == 0:
+            raise ValidationError('Укажите основный раздел')
+        if main_tags_count > 1:
+            raise ValidationError('Основный раздел может быть только один')
+        return result
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 0
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
